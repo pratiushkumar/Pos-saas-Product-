62a96dd8d9501b86578f2e01ceef6959bb13faf5
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2025-09-01 23:56:47 +0530

    testrunner

---
+++ Pos-System/src/test/java/com/product/Pos/System/PosSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.Pos.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
