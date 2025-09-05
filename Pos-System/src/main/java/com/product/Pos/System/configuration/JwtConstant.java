1e97678f7f74d0389d0ccde9b1ca5e0b7b722f6e
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2025-09-05 23:52:45 +0530

    Jwt Constant Creation

---
+++ Pos-System/src/main/java/com/product/Pos/System/configuration/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.product.Pos.System.configuration;
+
+public class JwtConstant {
+    //JWT 상수
+    public static final String JWT_SECRET = "meranaamtuntuntbabatuntunchalaakedekhle";
+    public static final String JWT_HEADER = "AUTHORIZATION";
+}
